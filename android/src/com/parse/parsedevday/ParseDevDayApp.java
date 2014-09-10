23506bdf671775d86e91c642a87f7654a198e6b8
Pranil Kanderi <pranil@mokriya.com>
2014-09-09 21:04:37 -0700

    App in working state, but needs lot of improvements

--- android/src/com/parse/parsedevday/ParseDevDayApp.java
+++ android/src/com/parse/parsedevday/ParseDevDayApp.java
@@ -1,5 +1,7 @@
 package com.parse.parsedevday;
 
+import android.app.Application;
+
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.PushService;
@@ -11,33 +13,33 @@ import com.parse.parsedevday.model.Talk;
 import com.parse.parsedevday.util.FavoritesNotificationScheduler;
 import com.parse.parsedevday.view.MainActivity;
 
-import android.app.Application;
-
 /**
- * The main app class mostly handles setting up global state, such as Parse keys.
+ * The main app class mostly handles setting up global state, such as Parse
+ * keys.
  */
 public class ParseDevDayApp extends Application {
-  public void onCreate() {
-    // Register subclasses for various kinds of Parse Objects.
-    ParseObject.registerSubclass(Room.class);
-    ParseObject.registerSubclass(Slot.class);
-    ParseObject.registerSubclass(Speaker.class);
-    ParseObject.registerSubclass(Talk.class);
+	public void onCreate() {
+		// Register subclasses for various kinds of Parse Objects.
+		ParseObject.registerSubclass(Room.class);
+		ParseObject.registerSubclass(Slot.class);
+		ParseObject.registerSubclass(Speaker.class);
+		ParseObject.registerSubclass(Talk.class);
 
-    // Initialize Parse with the application ID and client key.
-    /*Parse.initialize(this, "NN83LGZybDxGCxmGyA2b063eaYrLvCb2UUlJV7WB",
-        "sOgFGHbUYc2CoQ50aLYJ9ayA79ExSPT6553tVA3h");
-   */
-    Parse.initialize(this, "VP5fHQDm4PslLxdsjUgIWd4pZQSL73Tf1wZ2OhhV",
-        "iurEMEb2Ov4aX1nHp3GTDKMf9ppNSqMkHt5L4qY9J");
+		// Initialize Parse with the application ID and client key.
+		/*
+		 * Parse.initialize(this, "NN83LGZybDxGCxmGyA2b063eaYrLvCb2UUlJV7WB",
+		 * "sOgFGHbUYc2CoQ50aLYJ9ayA79ExSPT6553tVA3h");
+		 */
+		Parse.initialize(this, "VP5fHQDm4PslLxdsjUgIWd4pZQSL73Tf1wZ2OhhV", "urEMEb2Ov4aX1nHp3GTDKMf9ppNSqMkHt5L4qY9J");
 
-    // Enable the Parse push notification service for remote pushes.
-    PushService.setDefaultPushCallback(getApplicationContext(), MainActivity.class);
+		// Enable the Parse push notification service for remote pushes.
+		PushService.setDefaultPushCallback(getApplicationContext(), MainActivity.class);
 
-    // Setup the listener to handle local notifications for when talks start.
-    Favorites.get().addListener(new FavoritesNotificationScheduler(this));
+		// Setup the listener to handle local notifications for when talks
+		// start.
+		Favorites.get().addListener(new FavoritesNotificationScheduler(this));
 
-    // Read in the favorites from the local disk on this device.
-    Favorites.get().findLocally(this);
-  }
+		// Read in the favorites from the local disk on this device.
+		Favorites.get().findLocally(this);
+	}
 }
