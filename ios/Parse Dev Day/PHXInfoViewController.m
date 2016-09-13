de55f20e42f981ea7e980b3b87d1781950257792
Diogo Brito <diogowbrito@gmail.com>
2016-09-13 17:59:55 +0100

    Updated sponsors image.

--- ios/Parse Dev Day/PHXInfoViewController.m
+++ ios/Parse Dev Day/PHXInfoViewController.m
@@ -15,15 +15,6 @@
 
 @implementation PHXInfoViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (id)init {
     if (self = [super init]) {
         self.title = @"Sponsors";
@@ -32,51 +23,20 @@
     return self;
 }
 
-
 -(void) loadView{
-//    UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
-//    self.view = view;
-//    self.view.backgroundColor = [UIColor whiteColor];
-
-    //TODO: Get the spologo from the web.
+    UIView *view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    UIImageView *bgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Sponsors"]];
-    bgView.contentMode = UIViewContentModeScaleToFill;
-    self.view = bgView;
+    UIImageView *bgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"BG"]];
+    bgView.frame = view.bounds;
+    bgView.contentMode = UIViewContentModeCenter;
+    [view addSubview:bgView];
     
+    UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:view.bounds];
+    [backgroundImageView setImage:[UIImage imageNamed:@"Sponsors_v3.gif"]];
+    backgroundImageView.contentMode = UIViewContentModeScaleAspectFit;
+    [view addSubview:backgroundImageView];
     
-   // [self refreshLogo];
+    self.view = view;
 }
-
-//- (void)refreshLogo
-//{
-//    [PDDSponsors findAllInBackgroundWithBlock:^(NSArray *sponsors, NSError *error) {
-//      
-//    }];
-//}
-
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
